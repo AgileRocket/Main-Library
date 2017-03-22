c2c8ed5e5b1645d1a1c81afe8257b0b3e66401d8
Keith Kowalski <keith@swiftlybits.com>
2017-03-22 00:23:24 -0700

    Added:  Passed basic data from 'local model' to 'about us' page

--- main-library/src/main/java/rocket/agile/com/mainlibrary/AboutUsFragment.java
+++ main-library/src/main/java/rocket/agile/com/mainlibrary/AboutUsFragment.java
@@ -1,10 +1,14 @@
 package rocket.agile.com.mainlibrary;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 /**
@@ -23,5 +27,22 @@ public class AboutUsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about_us, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Set Header Text
+        TextView textView_title = (TextView) view.findViewById(R.id.aboutus_text_title);
+        textView_title.setText(dataManager.aboutUs_Header);
+
+        // Set Email Text
+        TextView textView_email = (TextView) view.findViewById(R.id.aboutus_text_email);
+        textView_email.setText(dataManager.aboutUs_Email);
+
+        // Set Body Text
+        TextView textView_body = (TextView) view.findViewById(R.id.aboutus_text_body);
+        textView_body.setText(dataManager.aboutUs_Body);
     }
 }
--- main-library/src/main/java/rocket/agile/com/mainlibrary/DataManager.java
+++ main-library/src/main/java/rocket/agile/com/mainlibrary/DataManager.java
@@ -1,5 +1,6 @@
 package rocket.agile.com.mainlibrary;
 
+import java.lang.reflect.*;
 /**
  * Created by keithkowalski on 3/21/17.
  */
@@ -12,8 +13,14 @@ public class DataManager {
 
     public int layoutValue = 0;
     public int phoneNumber = 01234567;
-    public String aboutUs = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+
+    // About Us Info
+    public String aboutUs_Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
                             "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure " +
                             "dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non " +
                             "proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    public String aboutUs_Email = "BusinessName@gmail.com";
+    public String aboutUs_Header = "Company Name";
 }
+
+
