c204f2484602fe23727b7128b43dcd0ba4d48b1a
Keith Kowalski <keith@agilerocket.com>
2017-09-08 16:50:14 -0700

    Menu items are placeable now; looking to add font awesome

--- main-library/src/main/java/rocket/agile/com/mainlibrary/activity/LayoutView_SideMenu.java
+++ main-library/src/main/java/rocket/agile/com/mainlibrary/activity/LayoutView_SideMenu.java
@@ -10,11 +10,14 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.SubMenu;
 import android.view.View;
+import android.widget.ImageView;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import io.realm.Realm;
@@ -36,8 +39,6 @@ import rocket.agile.com.mainlibrary.model.DataManager;
 public class LayoutView_SideMenu extends LayoutManager
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private final int MenuItem_EditId = 1, MenuItem_DeleteId = 0;
-
     // Call singleton class for data manager
     DataManager dataManager = DataManager.getInstance();
 
@@ -55,11 +56,8 @@ public class LayoutView_SideMenu extends LayoutManager
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
         // CUSTOM PRIMARY SETTINGS
-        createMenuItems();
+        pullMenuItemsFromNetworkCall();
 
         // PRIMARY COLOR
         View primaryBackground = findViewById(R.id.id_main);
@@ -68,93 +66,15 @@ public class LayoutView_SideMenu extends LayoutManager
         // PRIMARY HEADER COLOR AND TITLE
         primaryHeader.setBackgroundColor(Color.parseColor(dataManager.primaryHeaderColor));
         this.setTitle(dataManager.appName);
+
+
         if(this.getTitle() == null) {
             finish();
             startActivity(getIntent());
         }
     }
 
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        menu.add(0, 1, R.id.drawer_layout, "Agile Rocket").setIcon(R.drawable.agile_rocket_logo);
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.;
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.nav_drawer_main, menu);
-
-//        super.onCreateOptionsMenu(menu);
-//
-//        menu.add(0, 0, 0, "Option1").setShortcut('3', 'c');
-//        menu.add(0, 1, 0, "Option2").setShortcut('3', 'c');
-//        menu.add(0, 2, 0, "Option3").setShortcut('4', 's');
-//
-//        SubMenu sMenu = menu.addSubMenu(0, 3, 0, "SubMenu"); //If you want to add submenu
-//        sMenu.add(0, 4, 0, "SubOption1").setShortcut('5', 'z');
-//        sMenu.add(0, 5, 0, "SubOption2").setShortcut('5', 'z');
-
-//        if(enableAdd)
-//            menu.add(0, MENU_ADD, Menu.NONE, R.string.your-add-text).setIcon(R.drawable.your-add-icon);
-//        if(enableList)
-//            menu.add(0, MENU_LIST, Menu.NONE, R.string.your-list-text).setIcon(R.drawable.your-list-icon);
-//        if(enableRefresh)
-//            menu.add(0, MENU_REFRESH, Menu.NONE, R.string.your-refresh-text).setIcon(R.drawable.your-refresh-icon);
-//        if(enableLogin)
-//            menu.add(0, MENU_LOGIN, Menu.NONE, R.string.your-login-text).setIcon(R.drawable.your-login-icon);
-//        return super.onPrepareOptionsMenu(menu);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-//
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-//
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-        FragmentManager manager = getSupportFragmentManager();
-        WebsiteFragment websiteFragment = new WebsiteFragment();
-        AboutUsFragment aboutUsFragment = new AboutUsFragment();
-
-        // TODO:  Change from if-else to Switch statement
-
-        if (id == R.id.nav_website) {
-            manager.beginTransaction().replace(
-                    R.id.relative_layout_for_fragment,
-                    websiteFragment,
-                    websiteFragment.getTag()).commit();
-        } else if (id == R.id.nav_aboutUs) {
-            manager.beginTransaction().replace(
-                    R.id.relative_layout_for_fragment,
-                    aboutUsFragment,
-                    aboutUsFragment.getTag()).commit();
-        } else if (id == R.id.nav_home && manager.findFragmentById(R.id.relative_layout_for_fragment) != null) {
-            manager.beginTransaction().remove(manager.findFragmentById(R.id.relative_layout_for_fragment)).commit();
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    public void createMenuItems() {
+    public void pullMenuItemsFromNetworkCall() {
         // MENU BUTTONS TO CREATE
         Realm realm = Realm.getDefaultInstance();
         ArrayList<Object> actionList = new ArrayList<>();
@@ -169,6 +89,7 @@ public class LayoutView_SideMenu extends LayoutManager
         Object[] allActions = actionList.toArray();
 
         for(Object obj: allActions) {
+            String name = obj.getClass().toString();
             String rawName = obj.getClass().getSimpleName();
             if(rawName.endsWith("RealmProxy")) {
                 // Filter out class name
@@ -176,12 +97,15 @@ public class LayoutView_SideMenu extends LayoutManager
 
                 switch(className) {
                     case "ActionEmail":
+                        buildMenu(dataManager.actionEmail.get(0).getName(), "");
                         Log.d(dataManager.SIDE_MENU_TAG, "CREATE ACTION EMAIL BUTTON");
                         break;
                     case "ActionCall":
+                        buildMenu(dataManager.actionCall.first().getName(), "");
                         Log.d(dataManager.SIDE_MENU_TAG, "CREATE ACTION CALL BUTTON");
                         break;
                     case "ActionStaff":
+                        buildMenu(dataManager.actionStaff.first().getName(), "");
                         Log.d(dataManager.SIDE_MENU_TAG, "CREATE ACTION STAFF BUTTON");
                         break;
                     default:
@@ -190,5 +114,59 @@ public class LayoutView_SideMenu extends LayoutManager
             }
         }
         realm.close();
+    }
+
+    public void buildMenu(String title, String icon) {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        Menu menu = navigationView.getMenu();
+        // TODO: Resolve how to use Font Awesome with 'drawable'
+//        menu.add(title).setIcon(R.drawable.ic_menu_manage);
+        menu.add(title);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        String id2 = (String)item.getTitle();
+
+        FragmentManager manager = getSupportFragmentManager();
+        WebsiteFragment websiteFragment = new WebsiteFragment();
+        AboutUsFragment aboutUsFragment = new AboutUsFragment();
+
+        if (id == R.id.nav_home && manager.findFragmentById(R.id.relative_layout_for_fragment) != null) {
+            manager.beginTransaction().remove(manager.findFragmentById(R.id.relative_layout_for_fragment)).commit();
+        }
+
+        // TODO: See if id2 can reflect list of realm classes with data (data manager?)
+        switch(id2) {
+            case "Contact Us":
+                manager.beginTransaction().replace(
+                        R.id.relative_layout_for_fragment,
+                        aboutUsFragment,
+                        aboutUsFragment.getTag()).commit();
+            default: break;
+        }
+
+//        if (id == R.id.nav_website) {
+//            manager.beginTransaction().replace(
+//                    R.id.relative_layout_for_fragment,
+//                    websiteFragment,
+//                    websiteFragment.getTag()).commit();
+//        } else if (id2 == "Agile Rocket") {
+//            manager.beginTransaction().replace(
+//                    R.id.relative_layout_for_fragment,
+//                    aboutUsFragment,
+//                    aboutUsFragment.getTag()).commit();
+//        } else if (id == R.id.nav_home && manager.findFragmentById(R.id.relative_layout_for_fragment) != null) {
+//            manager.beginTransaction().remove(manager.findFragmentById(R.id.relative_layout_for_fragment)).commit();
+//        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
