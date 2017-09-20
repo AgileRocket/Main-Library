48ab95177c4a8ffd9623e8fabc0af589d3e44b94
Keith Kowalski <keith@agilerocket.com>
2017-09-20 17:49:46 -0400

    Adjusted back button to return to main menu from current fragment.

--- main-library/src/main/java/rocket/agile/com/mainlibrary/activity/LayoutManager.java
+++ main-library/src/main/java/rocket/agile/com/mainlibrary/activity/LayoutManager.java
@@ -3,30 +3,36 @@ package rocket.agile.com.mainlibrary.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
+
 import rocket.agile.com.mainlibrary.R;
 import rocket.agile.com.mainlibrary.model.ApplicationLifeCycleTracker;
 import rocket.agile.com.mainlibrary.model.DataManager;
 
 /**
  * Created by keithkowalski on 6/19/17.
- *
+ * <p>
  * Purpose:  Provide the layout chosen by the user
  * Function: A) Calls activity layout based on admin settings
- *           B) Base activity for all layouts (each layout extends LayoutManager) to access 'onResume', which applies to all
- *
+ * B) Base activity for all layouts (each layout extends LayoutManager) to access 'onResume', which applies to all
  */
 
 public class LayoutManager extends AppCompatActivity {
 
     // Context passed via constructor from calling class
     private static Context context;
-    public LayoutManager() {}
-    public LayoutManager(Context context) { this.context = context; }
+
+    public LayoutManager() {
+    }
+
+    public LayoutManager(Context context) {
+        this.context = context;
+    }
 
     // Call dataManager singleton
     DataManager dataManager = DataManager.getInstance();
@@ -73,7 +79,8 @@ public class LayoutManager extends AppCompatActivity {
             case 3:
                 context.startActivity(new Intent(context, LayoutView_Buttons_Long.class));
                 break;
-            default: break;
+            default:
+                break;
         }
     }
 
@@ -81,13 +88,16 @@ public class LayoutManager extends AppCompatActivity {
     @Override
     public void onBackPressed() {
 
-        if(dataManager.layoutValue == 0) {
+        FragmentManager manager = getSupportFragmentManager();
+
+        if (dataManager.layoutValue == 0) {
             DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
             if (drawer.isDrawerOpen(GravityCompat.START)) {
                 drawer.closeDrawer(GravityCompat.START);
-            } else {
-                exit();
+            } else if (manager.findFragmentById(R.id.relative_layout_for_fragment) != null) {
+                manager.beginTransaction().remove(manager.findFragmentById(R.id.relative_layout_for_fragment)).commit();
             }
+                exit();
         } else {
             exit();
         }
@@ -95,8 +105,9 @@ public class LayoutManager extends AppCompatActivity {
 
     // Exit method for non-active-side-menu
     private Boolean exit = false;
+
     private void exit() {
-        if(exit) {
+        if (exit) {
             moveTaskToBack(true); // finish activity
         } else {
             Toast.makeText(this, "Press Back again to Exit.",
